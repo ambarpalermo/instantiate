3147208185784b8675972fb5b250bce27c26095c
ambarpalermo <ambarpalermo06@gmail.com>
2022-05-04 15:49:13 -0300

    Initial commit

---
+++ Assets/CloneObject.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloneObject : MonoBehaviour
+{
+    public GameObject ObjectToClone;
+    
+
+    public void InstantiateObject()
+    {
+        int i = 0;
+        while(i < 3)
+        {
+            Instantiate(ObjectToClone);
+            i++;
+        }
+        
+    }
+}
